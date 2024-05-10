a1bfe3986c3e23b0e5932a046d695e41dd43b6af
t-nhi <135095690+t-nhi@users.noreply.github.com>
2024-05-10 18:01:38 +0700

    Add files via upload

---
+++ dangkyHDController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteQLHDTN.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.Language;
+using AspNetCore;
+
+namespace WebsiteQLHDTN.Controllers
+{
+    public class dangkyHDController : Controller
+    {
+
+        QLYHDTNContext db=new QLYHDTNContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult layTenBan(int hoatdongID)
+        {
+            List<SelectListItem> list = db.YeucauTnvs
+                .Where(model => model.Idhoatdong == hoatdongID)
+                .Select(ban=> new SelectListItem { Text=ban.Tenban, Value=ban.Tenban })
+                .ToList();
+            ViewBag.ListTenBan = list;
+            return View();
+        }
+
+        public IActionResult dangkythamgia(Tinhtrang model_quatrinhTG)
+        {
+            string uudiem = model_quatrinhTG.UuDiem;
+            string khuyetdiem = model_quatrinhTG.KhuyetDiem;
+            string ban = model_quatrinhTG.BanThamGia;
+
+            if (HttpContext.Session.GetString("uudiem") == null || HttpContext.Session.GetString("khuyetdiem") == null || string.IsNullOrEmpty(ban))
+            {
+                ViewBag.Message = "Vui lòng nhập đầy đủ dữ liệu";
+            }
+            else
+            {
+                var quatrinhTNV = new Tinhtrang
+                {
+                    UuDiem = uudiem,
+                    KhuyetDiem = khuyetdiem,
+                    BanThamGia = ban
+                };
+                db.Tinhtrangs.Add(quatrinhTNV);
+                db.SaveChanges();
+                ViewBag.Message = "Đăng ký tham gia thành công";
+            }
+            return View();
+        }
+
+    }
+
+    
+}
---
+++ thongtinHDController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+using WebsiteQLHDTN.Models;
+
+namespace WebsiteQLHDTN.Controllers
+{
+    
+    
+    public class thongtinHDController : Controller
+    {
+        QLYHDTNContext db = new QLYHDTNContext();
+        public IActionResult thongtinHD()
+        {
+            return View();
+        }
+        public IActionResult HienthithongtinHoatdong()
+        {
+            int? hoatdongID = HttpContext.Session.GetInt32("ID_HD");
+            var u = db.MotaHds.Where(model => model.Idhoatdong.Equals(hoatdongID)).Select(model => 
+                new { Tenhoatdong = model.Tenhoatdong, Thoigianbatdau = model.Thoigianbatdau, Thoigianketthuc = model.Thoigianketthuc,
+                    Diadiem = model.Diadiem, Linhvuc = model.Linhvuc, Thongdiep = model.Thongdiep, Muctieu = model.Muctieuhoatdong}).FirstOrDefault();
+            if (u != null)
+            {
+                ViewBag.Tenhoatdong = u.Tenhoatdong.ToString();
+                ViewBag.Thoigianbatdau = u.Thoigianbatdau.ToString();
+                ViewBag.Thoigianketthuc = u.Thoigianketthuc.ToString();
+                ViewBag.Diadiem=u.Diadiem.ToString();
+                ViewBag.Linhvuc = u.Linhvuc.ToString();
+                ViewBag.Thongdiep = u.Thongdiep.ToString();
+                ViewBag.Muctieu = u.Muctieu.ToString();
+            }
+            var soluong = db.Hoatdongs.Where(model => model.Idhoatdong.Equals(hoatdongID)).ToList().Count();
+            ViewBag.Tongsotnvcan = soluong;
+            var cacBan = db.YeucauTnvs
+                    .Where(model => model.Idhoatdong == hoatdongID)
+                    .Select(model => model.Tenban)
+                    .ToList();
+            ViewBag.ListCacban=cacBan;
+
+            return View();
+        }
+    }
+}
