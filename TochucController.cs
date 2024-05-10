112bc0ff5891872f2d06947d3f5de9db0cc8de52
t-nhi <135095690+t-nhi@users.noreply.github.com>
2024-05-11 00:28:10 +0700

    Add files via upload

---
+++ TochucController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteQLHDTN.Models;
+
+namespace WebsiteQLHDTN.Controllers
+{
+    public class TochucController : Controller
+    {
+        QLYHDTNContext db = new QLYHDTNContext();
+        
+        public IActionResult thongtinToChuc() 
+        {
+            int? userID = HttpContext.Session.GetInt32("UserID");
+            var u = db.Tochucs.Where(model => model.Idtochuc.Equals(userID)).Select(model => new { Ten = model.Ten, Email = model.Email, Sdt = model.Sdt, Diachi = model.Diachi }).FirstOrDefault();
+
+            if (u != null)
+            {
+                ViewBag.Ten = u.Ten.ToString();
+                ViewBag.Email = u.Email.ToString();
+                ViewBag.Sdt = u.Sdt.ToString();
+                ViewBag.Diachi = u.Diachi.ToString();                
+            }
+            var u1 = db.Motatochucs
+                .Where(model => model.Idtochuc.Equals(userID))
+                .Select(model => new { Gioithieu = model.Gioithieu, Thanhtuu = model.Thanhtuu, slHD = model.Hoatdongs.Count }).FirstOrDefault();
+            if (u1 != null)
+            {
+                ViewBag.Mota = u1.Gioithieu.ToString();
+                ViewBag.Thanhtuu = u1.Thanhtuu.ToString();
+                ViewBag.soluongHD = u1.slHD.ToString();
+
+            }    
+            return View();
+        }
+    }
+}
