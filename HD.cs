931fb958ca1efa7d9254d85ef67bae28a7d8c835
tramy984 <145307628+tramy984@users.noreply.github.com>
2024-05-11 07:51:34 +0700

    Add files via upload

---
+++ HD.cs
@@ -0,0 +1,34 @@
+﻿namespace PBL3_demo1.Viewmodel
+{
+    public class HD
+    {
+        public int Idhoatdong { get; set; }
+
+        public int? Idtochuc { get; set; }
+
+        public int? Tongsotnvcan { get; set; }
+
+        public int? Trangthai { get; set; }
+
+        public string? Tenhoatdong { get; set; }
+
+        public DateOnly? Thoigianbatdau { get; set; }
+
+        public DateOnly? Thoigianketthuc { get; set; }
+
+        public DateOnly? Thoigianbatdaudcs { get; set; }
+
+        public DateOnly? Thoigianketthucdcs { get; set; }
+
+        public DateOnly? Thoigianhuy { get; set; }
+
+        public string? Lydohuy { get; set; }
+
+        public string? Linhvuc { get; set; }
+
+        public string? Muctieuhoatdong { get; set; }
+
+        public string? Diadiem { get; set; }
+
+    }
+}
